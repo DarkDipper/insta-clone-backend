a30d75790b11449b64149f9ab3b4acf31965d6c6
Phong <phong.huynh@weai.vn>
2022-12-22 01:28:47 +0700

    Done API login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,17 +3,10 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 
-type userTemplate = {
-  username: string;
-  password: string;
-  email: string;
-};
 
 async function register(req: Request, res: Response) {
   try {
-    const { username, password, email }: userTemplate = req.body;
-    // const salt = await bcrypt.genSalt(10);
-    // throw new Error(`${password}`)
+    const { username, password, email } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
       user_name: username,
@@ -37,4 +30,35 @@ async function register(req: Request, res: Response) {
   }
 }
 
-export default { register };
+async function login(req:Request,res:Response) {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ user_name: username });
+    if (!user) {
+      return res.status(401).send({
+        stauts: "Failure",
+        message:"Cannot find user"
+      })
+    }
+    const match = await bcrypt.compare(password, user.pass_word)
+    if (!match) {
+      return res.status(401).send({
+        stauts: "Failure",
+        message: "Password not correct"
+      })
+    }
+    res.status(200).send({
+      status: "Success",
+      message:"Login successfully"
+    })
+  }
+  catch (e) {
+    if (e instanceof Error) {
+      res.status(500).send({ stauts: "Failure", message: e.message });
+    } else {
+      res.status(500).send({ stauts: "Failure", message: "Error unknow" });
+    }
+  }
+}
+
+export default { register,login };
