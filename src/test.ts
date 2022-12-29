e1e29ac38830863a45dd9d19173091ab93611c3d
Phong <phong.huynh@weai.vn>
2022-12-30 01:01:01 +0700

    Fix bug type env

--- src/test.ts
+++ src/test.ts
@@ -1,21 +1,7 @@
-import { generateToken } from "./utils/generateToken";
-import jwt  from "jsonwebtoken";
 import dotenv from "dotenv"
-import Mongoose from "mongoose";
 dotenv.config();
 
-// const user = {
-//   userName: "Trung",
-//   role: "admin",
-//   _id: new Mongoose.Types.ObjectId("123456")
-// }
+const secretKey:number = process.env.PORT
 
-// const SECRET_KEY_TOKEN = process.env.SECRET_KEY_TOKEN || "";
-
-// const userToken = generateToken(user,SECRET_KEY_TOKEN);
-
-// const decode = jwt.verify(userToken, SECRET_KEY_TOKEN);
-
-// console.log(userToken)
-
-// console.log(decode)
+console.log(typeof secretKey)
+console.log(secretKey)
